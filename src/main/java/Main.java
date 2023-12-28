04686b2ad6efe439ffa54024e87aac6fc863cf08
cpythonuser <adeelas2@illinois.edu>
2023-12-28 17:05:06 +0800

    Return 200 OK for root path & edge cases

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class Main {
+    public static final String PATH_SEPARATOR = "/";
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
@@ -26,15 +27,23 @@ public class Main {
             if (statusLine != null) {
                 try (PrintWriter output = new PrintWriter(clientSocket.getOutputStream())) {
                     String[] statusLineEntries = statusLine.split("\\s+");
-                    String[] requestPath;
-                    if (statusLineEntries.length > 1 && (requestPath = statusLineEntries[1].split("/")).length > 2 && Objects.equals(requestPath[1], "echo")) {
-                        String echoPath = String.join("/", Arrays.copyOfRange(requestPath, 2, requestPath.length));
+                    String[] requestPath = statusLineEntries[1].split(PATH_SEPARATOR);
+                    if (requestPath.length == 0) {
+                        // Return 200 for root path
+                        output.print("HTTP/1.1 200 OK\r\n\r\n");
+                    }
+                    else if (requestPath.length >= 2 && Objects.equals(requestPath[1], "echo")) {
+                        // Return substring after "echo" in url, e.g. GET /echo/abc/def should return abc/def with
+                        // Content-Length: 7, while GET /echo/ or GET /echo should return an empty string with
+                        // Content-Length: 0
+                        String echoPath = String.join(PATH_SEPARATOR, Arrays.copyOfRange(requestPath, 2, requestPath.length));
                         output.print("HTTP/1.1 200 OK\r\n\r\n");
                         output.print("Content-Type: text/plain\r\n");
                         output.print(String.format("Content-Length: %d\r\n", echoPath.length()));
                         output.print("\r\n");
                         output.print(String.format("%s\r\n", echoPath));
-                    } else {
+                    }
+                    else {
                         output.print("HTTP/1.1 404 Not Found\r\n\r\n");
                     }
                     output.flush();
