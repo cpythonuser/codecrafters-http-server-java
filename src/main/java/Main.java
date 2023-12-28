692ec67094110636baef14459fe42f39246d8dfb
cpythonuser <adeelas2@illinois.edu>
2023-12-28 17:18:40 +0800

    Fix response headers split lines

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ import java.util.Objects;
 
 public class Main {
     public static final String PATH_SEPARATOR = "/";
+
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
@@ -31,19 +32,14 @@ public class Main {
                     if (requestPath.length == 0) {
                         // Return 200 for root path
                         output.print("HTTP/1.1 200 OK\r\n\r\n");
-                    }
-                    else if (requestPath.length >= 2 && Objects.equals(requestPath[1], "echo")) {
+                    } else if (requestPath.length >= 2 && Objects.equals(requestPath[1], "echo")) {
                         // Return substring after "echo" in url, e.g. GET /echo/abc/def should return abc/def with
                         // Content-Length: 7, while GET /echo/ or GET /echo should return an empty string with
                         // Content-Length: 0
                         String echoPath = String.join(PATH_SEPARATOR, Arrays.copyOfRange(requestPath, 2, requestPath.length));
-                        output.print("HTTP/1.1 200 OK\r\n\r\n");
-                        output.print("Content-Type: text/plain\r\n");
-                        output.print(String.format("Content-Length: %d\r\n", echoPath.length()));
-                        output.print("\r\n");
-                        output.print(String.format("%s\r\n", echoPath));
-                    }
-                    else {
+                        String responseBody = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + String.format("Content-Length: %d\r\n\r\n", echoPath.length()) + echoPath;
+                        output.print(responseBody);
+                    } else {
                         output.print("HTTP/1.1 404 Not Found\r\n\r\n");
                     }
                     output.flush();
